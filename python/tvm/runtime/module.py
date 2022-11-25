43352d07c2d561eabeaee0203aa64f56bb4353de
tqchen <tianqi.tchen@gmail.com>
2022-11-25 13:44:32 -0500

    Hotpatch

--- python/tvm/runtime/module.py
+++ python/tvm/runtime/module.py
@@ -522,7 +522,11 @@ class Module(object):
 
         if self.imported_modules:
             if enabled("llvm") and llvm_target_string:
+<<<<<<< HEAD
                 path_obj = os.path.join(workspace_dir, f"devc.{global_object_format}")
+=======
+                path_obj = os.path.join(workspace_dir, "devc.o")
+>>>>>>> Hotpatch
                 m = _ffi_api.ModulePackImportsToLLVM(self, is_system_lib, llvm_target_string)
                 m.save(path_obj)
                 files.append(path_obj)
